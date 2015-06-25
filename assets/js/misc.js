b70beeed95a9d4a0a8b8ca01382b9c58e02aaed8
ShalomYiblet <shalom567@gmail.com>
2015-06-24 23:48:28 -0400

    added some motion in to the background

---
+++ assets/js/misc.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Shalom Yiblet on 6/19/2015.
+ */
+$.fn.textWidth = function(){
+    var html_org = $(this).html();
+    var html_calc = '<span>' + html_org + '</span>';
+    $(this).html(html_calc);
+    var width = $(this).find('span:first').width();
+    $(this).html(html_org);
+    return width;
+};
